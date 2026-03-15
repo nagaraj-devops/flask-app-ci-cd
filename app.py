619007c0726b68857d40e9dcfd24ae58b66afd47
Nagaraj Sajjan <nagaraj@webenza.com>
2026-03-15 12:52:05 +0530

    [2026-03-15 12:43] working pipeline, final build

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Define the root route
 @app.route("/", methods=["GET"])
 def home():
-    return "<h1>Hello, World!</h1><p>This is my first DevOps-powered Flask app.</p>"
+    return "<h1>Hello, World!</h1><p>This is my first DevOps-powered Flask app. Successfully deployed! After so many pipeline failures</p>"
 
 if __name__ == "__main__":
     # Run the application in debug mode
